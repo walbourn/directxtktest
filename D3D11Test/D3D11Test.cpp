47eb37273a85f1ff760d1b042fe0a9273ced9932
Chuck Walbourn <chuckw@windows.microsoft.com>
2015-06-04 17:21:05 -0700

    Added D3D11Test

---
+++ D3D11Test/D3D11Test.cpp
@@ -0,0 +1,178 @@
+//--------------------------------------------------------------------------------------
+// File: D3D11Test.cpp
+//
+// Developer unit test for basic Direct3D 11 support
+//
+// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+// PARTICULAR PURPOSE.
+//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+//
+// http://go.microsoft.com/fwlink/?LinkId=248929
+//--------------------------------------------------------------------------------------
+
+#include "CommonStates.h"
+#include "DDSTextureLoader.h"
+#include "DirectXHelpers.h"
+#include "Effects.h"
+#include "GeometricPrimitive.h"
+#include "Model.h"
+#include "PrimitiveBatch.h"
+#include "ScreenGrab.h"
+#include "SpriteBatch.h"
+#include "SpriteFont.h"
+#include "VertexTypes.h"
+#include "WICTextureLoader.h"
+
+#include <wrl/client.h>
+
+using namespace DirectX;
+using Microsoft::WRL::ComPtr;
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    switch (msg)
+    {
+        case WM_CLOSE:
+            DestroyWindow(hwnd);
+            return 0;
+
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+
+        default:
+            return DefWindowProc(hwnd, msg, wParam, lParam);
+    }
+}
+
+
+int WINAPI wWinMain( _In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPWSTR, _In_ int nCmdShow )
+{
+    HRESULT hr;
+
+    wchar_t *const className = L"TestWindowClass";
+
+    WNDCLASSEX wndClass = { 0 };
+
+    wndClass.cbSize = sizeof(WNDCLASSEX);
+    wndClass.lpfnWndProc = WndProc;
+    wndClass.hInstance = hInstance;
+    wndClass.lpszClassName = className;
+    wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+
+    RegisterClassEx(&wndClass);
+
+    HWND hwnd = CreateWindowEx(WS_EX_CLIENTEDGE, className, L"Test Window", WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU, CW_USEDEFAULT, CW_USEDEFAULT, 640, 640, NULL, NULL, hInstance, NULL);
+
+    ShowWindow(hwnd, nCmdShow);
+    UpdateWindow(hwnd);
+
+    RECT client;
+    GetClientRect(hwnd, &client);
+
+    ComPtr<ID3D11Device> device;
+    ComPtr<ID3D11DeviceContext> context;
+    ComPtr<IDXGISwapChain> swapChain;
+    ComPtr<ID3D11Texture2D> backBufferTexture;
+    ComPtr<ID3D11RenderTargetView> backBuffer;
+
+    DXGI_SWAP_CHAIN_DESC swapChainDesc = { 0 };
+
+    swapChainDesc.BufferCount = 1;
+    swapChainDesc.BufferDesc.Width = client.right;
+    swapChainDesc.BufferDesc.Height = client.bottom;
+    swapChainDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+    swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT | DXGI_USAGE_BACK_BUFFER;
+    swapChainDesc.OutputWindow = hwnd;
+    swapChainDesc.SampleDesc.Count = 1;
+    swapChainDesc.Windowed = TRUE;
+
+    D3D_FEATURE_LEVEL featureLevel = D3D_FEATURE_LEVEL_10_0;
+    
+    DWORD d3dFlags = 0;
+#ifdef _DEBUG
+    d3dFlags |= D3D11_CREATE_DEVICE_DEBUG;
+#endif
+
+    if (FAILED(hr = D3D11CreateDeviceAndSwapChain(nullptr, D3D_DRIVER_TYPE_HARDWARE, nullptr, d3dFlags, &featureLevel, 1, D3D11_SDK_VERSION, &swapChainDesc, &swapChain, &device, nullptr, &context)))
+        return 1;
+
+#ifndef NDEBUG
+    ComPtr<ID3D11Debug> d3dDebug;
+    hr = device.As(&d3dDebug);
+    if (SUCCEEDED(hr))
+    {
+        ComPtr<ID3D11InfoQueue> d3dInfoQueue;
+        hr = d3dDebug.As(&d3dInfoQueue);
+        if (SUCCEEDED(hr))
+        {
+#ifdef _DEBUG
+            d3dInfoQueue->SetBreakOnSeverity(D3D11_MESSAGE_SEVERITY_CORRUPTION, true);
+            d3dInfoQueue->SetBreakOnSeverity(D3D11_MESSAGE_SEVERITY_ERROR, true);
+#endif
+            D3D11_MESSAGE_ID hide[] =
+            {
+                D3D11_MESSAGE_ID_SETPRIVATEDATA_CHANGINGPARAMS,
+            };
+            D3D11_INFO_QUEUE_FILTER filter;
+            memset(&filter, 0, sizeof(filter));
+            filter.DenyList.NumIDs = _countof(hide);
+            filter.DenyList.pIDList = hide;
+            d3dInfoQueue->AddStorageFilterEntries(&filter);
+        }
+    }
+#endif
+
+    if (FAILED(hr = swapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (void**)&backBufferTexture)))
+        return 1;
+
+    CD3D11_RENDER_TARGET_VIEW_DESC renderTargetViewDesc(D3D11_RTV_DIMENSION_TEXTURE2D);
+
+    if (FAILED(hr = device->CreateRenderTargetView(backBufferTexture.Get(), &renderTargetViewDesc, &backBuffer)))
+        return 1;
+   
+    SetDebugObjectName(backBuffer.Get(), "BackBuffer");
+
+    bool quit = false;
+
+    CD3D11_VIEWPORT vp( 0.f, 0.f, (float)client.right, (float)client.bottom, 0.f, 1.f );
+
+    context->RSSetViewports(1, &vp);
+
+    context->OMSetRenderTargets(1, backBuffer.GetAddressOf(), nullptr);
+
+    size_t frame = 0;
+
+    while (!quit)
+    {
+        MSG msg;
+
+        while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+        {
+            if (msg.message == WM_QUIT)
+                quit = true;
+
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+
+        static float t = 0.0f;
+        static uint64_t timeStart = 0;
+        uint64_t timeCur = GetTickCount64();
+        if( timeStart == 0 )
+            timeStart = timeCur;
+        t = ( timeCur - timeStart ) / 1000.0f;
+
+        context->ClearRenderTargetView(backBuffer.Get(), Colors::CornflowerBlue);
+
+        // Scene is blank for this test
+
+        swapChain->Present(1, 0);
+        ++frame;
+    }
+
+    return 0;
+}
