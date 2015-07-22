25430f947f7de4099740f8197279eccc2b290189
Chuck Walbourn <chuckw@windows.microsoft.com>
2015-07-22 11:48:51 -0700

    Added relative mode vs. absolute mode to Mouse

--- MouseTest/MouseTest.cpp
+++ MouseTest/MouseTest.cpp
@@ -17,16 +17,23 @@
 #define WIN32_LEAN_AND_MEAN
 #endif
 
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
+
 #include <windows.h>
 #include "Mouse.h"
 #include "CommonStates.h"
 #include "VertexTypes.h"
 #include "DirectXColors.h"
+#include "DDSTextureLoader.h"
 #include "WICTextureLoader.h"
 #include "ScreenGrab.h"
 #include "SpriteBatch.h"
 #include "SpriteFont.h"
+#include "GeometricPrimitive.h"
 
+#include <algorithm>
 #include <stdio.h>
 
 #pragma warning(push)
@@ -39,11 +46,12 @@ using Microsoft::WRL::ComPtr;
 
 using namespace DirectX;
 
+static const XMVECTORF32 START_POSITION = { 0.f, -1.5f, 0.f, 0.f };
+static const XMVECTORF32 ROOM_BOUNDS = { 8.f, 6.f, 12.f, 0.f };
+
 std::unique_ptr<SpriteBatch> g_spriteBatch;
 std::unique_ptr<Mouse> g_mouse;
-
-// Build for LH vs. RH coords
-//#define LH_COORDS
+std::unique_ptr<GeometricPrimitive> g_room;
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
@@ -57,6 +65,8 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
             PostQuitMessage(0);
             return 0;
 
+        case WM_ACTIVATEAPP:
+        case WM_INPUT:
         case WM_MOUSEMOVE:
         case WM_LBUTTONDOWN:
         case WM_LBUTTONUP:
@@ -67,6 +77,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
         case WM_MOUSEWHEEL:
         case WM_XBUTTONDOWN:
         case WM_XBUTTONUP:
+        case WM_MOUSEHOVER:
             Mouse::ProcessMessage(msg, wParam, lParam);
             break;
     }
@@ -92,7 +103,7 @@ int WINAPI wWinMain( _In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPWSTR, 
     RegisterClassEx(&wndClass);
 
     HWND hwnd = CreateWindowEx(WS_EX_CLIENTEDGE, className, L"Test Window", WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU,
-                               CW_USEDEFAULT, CW_USEDEFAULT, 1024, 768, nullptr, nullptr, hInstance, nullptr);
+                               CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720, nullptr, nullptr, hInstance, nullptr);
 
     ShowWindow(hwnd, nCmdShow);
     UpdateWindow(hwnd);
@@ -136,17 +147,59 @@ int WINAPI wWinMain( _In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPWSTR, 
     if (FAILED(hr = device->CreateRenderTargetView(backBufferTexture.Get(), &renderTargetViewDesc, &backBuffer)))
         return 1;
 
+    D3D11_TEXTURE2D_DESC depthStencilDesc = { 0 };
+
+    depthStencilDesc.Width = client.right;
+    depthStencilDesc.Height = client.bottom;
+    depthStencilDesc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+    depthStencilDesc.SampleDesc.Count = 1;
+    depthStencilDesc.Usage = D3D11_USAGE_DEFAULT;
+    depthStencilDesc.BindFlags = D3D11_BIND_DEPTH_STENCIL;
+    depthStencilDesc.MipLevels = 1;
+    depthStencilDesc.ArraySize = 1;
+
+    ComPtr<ID3D11Texture2D> depthStencilTexture;
+    if (FAILED(device->CreateTexture2D(&depthStencilDesc, nullptr, &depthStencilTexture)))
+        return 1;
+
+    D3D11_DEPTH_STENCIL_VIEW_DESC depthStencilViewDesc;
+    ZeroMemory(&depthStencilViewDesc, sizeof(D3D11_DEPTH_STENCIL_VIEW_DESC));
+    depthStencilViewDesc.Format = DXGI_FORMAT_UNKNOWN;
+    depthStencilViewDesc.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2D;
+    depthStencilViewDesc.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2D;
+
+    ComPtr<ID3D11DepthStencilView> depthStencil;
+    if (FAILED(device->CreateDepthStencilView(depthStencilTexture.Get(), &depthStencilViewDesc, &depthStencil)))
+        return 1;
+
     CommonStates states(device.Get());
 
     g_spriteBatch.reset(new SpriteBatch(context.Get()));
+
+    ComPtr<ID3D11ShaderResourceView> roomTex;
+    if (FAILED(hr = CreateDDSTextureFromFile(device.Get(), L"texture.dds", nullptr, roomTex.GetAddressOf())))
+        return 1;
 
     ComPtr<ID3D11ShaderResourceView> cursor;
     if (FAILED(hr = CreateWICTextureFromFile(device.Get(), L"arrow.png", nullptr, cursor.GetAddressOf())))
         return 1;
 
+    g_room = GeometricPrimitive::CreateBox(context.Get(), XMFLOAT3(ROOM_BOUNDS[0], ROOM_BOUNDS[1], ROOM_BOUNDS[2]), false, true);
+
+    XMVECTOR cameraPos = START_POSITION;
+
+    static const XMVECTORF32 up = { 0.f, 1.f, 0.f, 0.f };
+
+    XMMATRIX proj = XMMatrixPerspectiveFovRH(XMConvertToRadians(70.f), float(client.right) / float(client.bottom), 0.01f, 100.f);
+    XMMATRIX world = XMMatrixIdentity();
+
+    float pitch = 0;
+    float yaw = 0;
+
     SpriteFont comic(device.Get(), L"comic.spritefont");
 
     g_mouse.reset( new Mouse );
+    g_mouse->SetWindow(hwnd);
 
     // Singleton test
     {
@@ -176,19 +229,14 @@ int WINAPI wWinMain( _In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPWSTR, 
 
     context->RSSetViewports(1, &vp);
 
-    context->OMSetRenderTargets(1, backBuffer.GetAddressOf(), nullptr);
-
-#ifdef LH_COORDS
-    context->RSSetState( states.CullCounterClockwise() );
-#else
-    context->RSSetState( states.CullClockwise() );
-#endif
+    context->OMSetRenderTargets(1, backBuffer.GetAddressOf(), depthStencil.Get());
 
     size_t frame = 0;
 
     Mouse::ButtonStateTracker tracker;
 
     const wchar_t * lastStr = nullptr;
+    Mouse::Mode lastMode = Mouse::MODE_ABSOLUTE;
     while (!quit)
     {
         MSG msg;
@@ -201,11 +249,62 @@ int WINAPI wWinMain( _In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPWSTR, 
             if (msg.message == WM_KEYDOWN && msg.wParam == VK_HOME)
                 g_mouse->ResetScrollWheelValue();
 
+            if (msg.message == WM_KEYDOWN && msg.wParam == VK_END)
+            {
+                if (lastMode == Mouse::MODE_ABSOLUTE)
+                {
+                    g_mouse->SetMode(Mouse::MODE_RELATIVE);
+                }
+                else
+                {
+                    g_mouse->SetMode(Mouse::MODE_ABSOLUTE);
+                }
+            }
+
             TranslateMessage(&msg);
             DispatchMessage(&msg);
         }
 
+        context->ClearRenderTargetView(backBuffer.Get(), Colors::CornflowerBlue);
+        context->ClearDepthStencilView(depthStencil.Get(), D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1, 0);
+
+        float y = sinf(pitch);        // vertical
+        float r = cosf(pitch);        // in the plane
+        float z = r*cosf(yaw);        // fwd-back
+        float x = r*sinf(yaw);        // left-right
+
+        XMVECTOR lookAt = XMVectorAdd(cameraPos, XMVectorSet(x, y, z, 0.f));
+
+        XMMATRIX view = XMMatrixLookAtRH(cameraPos, lookAt, up);
+
+        g_room->Draw(world, view, proj, Colors::White, roomTex.Get());
+
         auto mouse = g_mouse->GetState();
+        lastMode = mouse.positionMode;
+
+        if (mouse.positionMode == Mouse::MODE_RELATIVE)
+        {
+            static const XMVECTORF32 ROTATION_GAIN = { 0.004, 0.004, 0.f, 0.f };
+            XMVECTOR delta = XMVectorSet(float(mouse.x), float(mouse.y), 0.f, 0.f) * ROTATION_GAIN;
+
+            pitch -= XMVectorGetY(delta);
+            yaw -= XMVectorGetX(delta);
+
+            // limit pitch to straight up or straight down
+            float limit = XM_PI/ 2.0f - 0.01f;
+            pitch = std::max(-limit, pitch);
+            pitch = std::min(+limit, pitch);
+
+            // keep longitude in sane range by wrapping
+            if (yaw > XM_PI)
+            {
+                yaw -= XM_PI * 2.0f;
+            }
+            else if (yaw < -XM_PI)
+            {
+                yaw += XM_PI * 2.0f;
+            }
+        }
 
         tracker.Update(mouse);
 
@@ -230,7 +329,14 @@ int WINAPI wWinMain( _In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPWSTR, 
         else if (tracker.xButton2 == Mouse::ButtonStateTracker::ButtonState::RELEASED)
             lastStr = L"XButton2 was released";
 
-        context->ClearRenderTargetView(backBuffer.Get(), Colors::CornflowerBlue);
+        if (tracker.leftButton == Mouse::ButtonStateTracker::ButtonState::PRESSED)
+        {
+            g_mouse->SetMode(Mouse::MODE_RELATIVE);
+        }
+        else if (tracker.leftButton == Mouse::ButtonStateTracker::ButtonState::RELEASED)
+        {
+            g_mouse->SetMode(Mouse::MODE_ABSOLUTE);
+        }
 
         XMVECTOR xsize = comic.MeasureString( L"X" );
 
@@ -263,12 +369,18 @@ int WINAPI wWinMain( _In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPWSTR, 
             comic.DrawString(g_spriteBatch.get(), buff, pos, Colors::White);
         }
 
+        comic.DrawString(g_spriteBatch.get(), (mouse.positionMode == Mouse::MODE_RELATIVE) ? L"Relative" : L"Absolute",
+                         XMFLOAT2(50, 550), Colors::Blue);
+
         if ( lastStr )
         {
-            comic.DrawString( g_spriteBatch.get(), lastStr, XMFLOAT2( 50, 650 ), Colors::Yellow );
+            comic.DrawString( g_spriteBatch.get(), lastStr, XMFLOAT2( 50, 600 ), Colors::Yellow );
         }
 
-        g_spriteBatch->Draw(cursor.Get(), XMFLOAT2( (float)mouse.x, (float)mouse.y));
+        if (mouse.positionMode == Mouse::MODE_ABSOLUTE)
+        {
+            g_spriteBatch->Draw(cursor.Get(), XMFLOAT2((float) mouse.x, (float) mouse.y));
+        }
 
         g_spriteBatch->End();
 
