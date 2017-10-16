6f450d22125640d7cc6a0e43d475b9924b6376f8
Chuck Walbourn <chuckw@windows.microsoft.com>
2017-10-16 10:40:35 -0700

    Test update

--- Common/MainPC.cpp
+++ Common/MainPC.cpp
@@ -151,8 +151,15 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     switch (message)
     {
     case WM_PAINT:
-        hdc = BeginPaint(hWnd, &ps);
-        EndPaint(hWnd, &ps);
+        if (s_in_sizemove && game)
+        {
+            game->Tick();
+        }
+        else
+        {
+            hdc = BeginPaint(hWnd, &ps);
+            EndPaint(hWnd, &ps);
+        }
         break;
 
     case WM_SIZE:
