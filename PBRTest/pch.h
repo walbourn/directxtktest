c99909f7f22332c0cf28c9670749cbde4655b32a
Chuck Walbourn <chuckw@windows.microsoft.com>
2017-11-28 13:15:58 -0800

    Added more pbrtest configs

--- PBRTest/pch.h
+++ PBRTest/pch.h
@@ -55,14 +55,7 @@
 #include <dxgi1_6.h>
 #else
 #include <d3d11_1.h>
-
-#if (_WIN32_WINNT >= 0x0A00 /*_WIN32_WINNT_WIN10*/)
-#if defined(NTDDI_WIN10_RS2)
 #include <dxgi1_6.h>
-#else
-#include <dxgi1_5.h>
-#endif
-#endif
 #endif
 
 #ifdef _DEBUG
