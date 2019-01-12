26dbe5f1b927f0884633a4f43e611ed9002d424f
Chuck Walbourn <chuckw@windows.microsoft.com>
2019-01-11 17:24:53 -0800

    New test

---
+++ PBRModelTest/Game.cpp
@@ -0,0 +1,329 @@
+//--------------------------------------------------------------------------------------
+// File: Game.cpp
+//
+// Developer unit test for DirectXTK PBR Model Test
+//
+// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+// PARTICULAR PURPOSE.
+//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+//
+// http://go.microsoft.com/fwlink/?LinkId=248929
+//--------------------------------------------------------------------------------------
+
+#include "pch.h"
+#include "Game.h"
+
+//#define USE_FAST_SEMANTICS
+
+// Build for LH vs. RH coords
+//#define LH_COORDS
+
+// For UWP/PC, this tests using a linear F16 swapchain intead of HDR10
+//#define TEST_HDR_LINEAR
+
+extern void ExitGame();
+
+using namespace DirectX;
+using namespace DirectX::SimpleMath;
+
+using Microsoft::WRL::ComPtr;
+
+// Constructor.
+Game::Game() noexcept(false)
+{
+#ifdef TEST_HDR_LINEAR
+    const DXGI_FORMAT c_DisplayFormat = DXGI_FORMAT_R16G16B16A16_FLOAT;
+#else
+    const DXGI_FORMAT c_DisplayFormat = DXGI_FORMAT_R10G10B10A2_UNORM;
+#endif
+
+#if defined(_XBOX_ONE) && defined(_TITLE)
+    m_deviceResources = std::make_unique<DX::DeviceResources>(
+        DXGI_FORMAT_R10G10B10A2_UNORM, DXGI_FORMAT_D32_FLOAT, 2,
+        DX::DeviceResources::c_Enable4K_UHD
+#ifdef USE_FAST_SEMANTICS
+        | DX::DeviceResources::c_FastSemantics
+#endif
+        | DX::DeviceResources::c_EnableHDR);
+#elif defined(WINAPI_FAMILY) && (WINAPI_FAMILY == WINAPI_FAMILY_APP)
+    m_deviceResources = std::make_unique<DX::DeviceResources>(
+        c_DisplayFormat, DXGI_FORMAT_D32_FLOAT, 2, D3D_FEATURE_LEVEL_10_0,
+        DX::DeviceResources::c_EnableHDR | DX::DeviceResources::c_Enable4K_Xbox);
+#else
+    m_deviceResources = std::make_unique<DX::DeviceResources>(
+        c_DisplayFormat, DXGI_FORMAT_D32_FLOAT, 2, D3D_FEATURE_LEVEL_10_0,
+        DX::DeviceResources::c_EnableHDR);
+#endif
+
+#if !defined(_XBOX_ONE) || !defined(_TITLE)
+    m_deviceResources->RegisterDeviceNotify(this);
+#endif
+
+    // Set up for HDR rendering.
+    m_hdrScene = std::make_unique<DX::RenderTexture>(DXGI_FORMAT_R16G16B16A16_FLOAT);
+}
+
+// Initialize the Direct3D resources required to run.
+void Game::Initialize(
+#if !defined(WINAPI_FAMILY) || (WINAPI_FAMILY == WINAPI_FAMILY_DESKTOP_APP) 
+    HWND window,
+#else
+    IUnknown* window,
+#endif
+    int width, int height, DXGI_MODE_ROTATION rotation)
+{
+    m_gamePad = std::make_unique<GamePad>();
+    m_keyboard = std::make_unique<Keyboard>();
+
+#if defined(_XBOX_ONE) && defined(_TITLE)
+    UNREFERENCED_PARAMETER(rotation);
+    UNREFERENCED_PARAMETER(width);
+    UNREFERENCED_PARAMETER(height);
+    m_deviceResources->SetWindow(window);
+    m_keyboard->SetWindow(reinterpret_cast<ABI::Windows::UI::Core::ICoreWindow*>(window));
+#elif defined(WINAPI_FAMILY) && (WINAPI_FAMILY == WINAPI_FAMILY_APP)
+    m_deviceResources->SetWindow(window, width, height, rotation);
+    m_keyboard->SetWindow(reinterpret_cast<ABI::Windows::UI::Core::ICoreWindow*>(window));
+#else
+    UNREFERENCED_PARAMETER(rotation);
+    m_deviceResources->SetWindow(window, width, height);
+#endif
+
+    m_deviceResources->CreateDeviceResources();
+    CreateDeviceDependentResources();
+
+    m_deviceResources->CreateWindowSizeDependentResources();
+    CreateWindowSizeDependentResources();
+}
+
+#pragma region Frame Update
+// Executes the basic game loop.
+void Game::Tick()
+{
+    m_timer.Tick([&]()
+    {
+        Update(m_timer);
+    });
+
+    Render();
+}
+
+// Updates the world.
+void Game::Update(DX::StepTimer const&)
+{
+    auto pad = m_gamePad->GetState(0);
+    auto kb = m_keyboard->GetState();
+    if (kb.Escape || (pad.IsConnected() && pad.IsViewPressed()))
+    {
+        ExitGame();
+    }
+}
+#pragma endregion
+
+#pragma region Frame Render
+// Draws the scene.
+void Game::Render()
+{
+    // Don't try to render anything before the first Update.
+    if (m_timer.GetFrameCount() == 0)
+    {
+        return;
+    }
+
+#if defined(_XBOX_ONE) && defined(_TITLE)
+    m_deviceResources->Prepare();
+#endif
+
+    Clear();
+
+    auto context = m_deviceResources->GetD3DDeviceContext();
+
+    // TODO: Add your rendering code here.
+
+    // Tonemap the frame.
+#if defined(_XBOX_ONE) && defined(_TITLE)
+    m_hdrScene->EndScene(context);
+#endif
+
+#if defined(_XBOX_ONE) && defined(_TITLE)
+    ID3D11RenderTargetView* renderTargets[2] = { m_deviceResources->GetRenderTargetView(), m_deviceResources->GetGameDVRRenderTargetView() };
+    context->OMSetRenderTargets(2, renderTargets, nullptr);
+#else
+    auto renderTarget = m_deviceResources->GetRenderTargetView();
+    context->OMSetRenderTargets(1, &renderTarget, nullptr);
+
+    switch (m_deviceResources->GetColorSpace())
+    {
+    default:
+        m_toneMap->SetOperator(ToneMapPostProcess::ACESFilmic);
+        m_toneMap->SetTransferFunction((m_deviceResources->GetBackBufferFormat() == DXGI_FORMAT_R16G16B16A16_FLOAT) ? ToneMapPostProcess::Linear : ToneMapPostProcess::SRGB);
+        break;
+
+    case DXGI_COLOR_SPACE_RGB_FULL_G2084_NONE_P2020:
+        m_toneMap->SetOperator(ToneMapPostProcess::None);
+        m_toneMap->SetTransferFunction(ToneMapPostProcess::ST2084);
+        break;
+
+    case DXGI_COLOR_SPACE_RGB_FULL_G10_NONE_P709:
+        m_toneMap->SetOperator(ToneMapPostProcess::None);
+        m_toneMap->SetTransferFunction(ToneMapPostProcess::Linear);
+        break;
+    }
+#endif
+
+    m_toneMap->Process(context);
+
+    ID3D11ShaderResourceView* nullsrv[] = { nullptr };
+    context->PSSetShaderResources(0, 1, nullsrv);
+    // Show the new frame.
+    m_deviceResources->Present();
+
+#if defined(_XBOX_ONE) && defined(_TITLE)
+    m_graphicsMemory->Commit();
+#endif
+}
+
+// Helper method to clear the back buffers.
+void Game::Clear()
+{
+    // Clear the views.
+    auto context = m_deviceResources->GetD3DDeviceContext();
+    auto renderTarget = m_hdrScene->GetRenderTargetView();
+    auto depthStencil = m_deviceResources->GetDepthStencilView();
+
+    XMVECTORF32 color;
+    color.v = XMColorSRGBToRGB(Colors::CornflowerBlue);
+    context->ClearRenderTargetView(renderTarget, color);
+    context->ClearDepthStencilView(depthStencil, D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
+    context->OMSetRenderTargets(1, &renderTarget, depthStencil);
+
+    // Set the viewport.
+    auto viewport = m_deviceResources->GetScreenViewport();
+    context->RSSetViewports(1, &viewport);
+}
+#pragma endregion
+
+#pragma region Message Handlers
+// Message handlers
+void Game::OnActivated()
+{
+}
+
+void Game::OnDeactivated()
+{
+}
+
+void Game::OnSuspending()
+{
+#if defined(_XBOX_ONE) && defined(_TITLE)
+    auto context = m_deviceResources->GetD3DDeviceContext();
+    context->Suspend(0);
+#endif
+}
+
+void Game::OnResuming()
+{
+#if defined(_XBOX_ONE) && defined(_TITLE)
+    auto context = m_deviceResources->GetD3DDeviceContext();
+    context->Resume();
+#endif
+
+    m_timer.ResetElapsedTime();
+}
+
+#if !defined(WINAPI_FAMILY) || (WINAPI_FAMILY == WINAPI_FAMILY_DESKTOP_APP) 
+void Game::OnWindowMoved()
+{
+    auto r = m_deviceResources->GetOutputSize();
+    m_deviceResources->WindowSizeChanged(r.right, r.bottom);
+}
+#endif
+
+#if !defined(_XBOX_ONE) || !defined(_TITLE)
+void Game::OnWindowSizeChanged(int width, int height, DXGI_MODE_ROTATION rotation)
+{
+#if defined(WINAPI_FAMILY) && (WINAPI_FAMILY == WINAPI_FAMILY_APP)
+    if (!m_deviceResources->WindowSizeChanged(width, height, rotation))
+        return;
+#else
+    UNREFERENCED_PARAMETER(rotation);
+    if (!m_deviceResources->WindowSizeChanged(width, height))
+        return;
+#endif
+
+    CreateWindowSizeDependentResources();
+}
+#endif
+
+#if defined(WINAPI_FAMILY) && (WINAPI_FAMILY == WINAPI_FAMILY_APP)
+void Game::ValidateDevice()
+{
+    m_deviceResources->ValidateDevice();
+}
+#endif
+
+// Properties
+void Game::GetDefaultSize(int& width, int& height) const
+{
+    width = 1280;
+    height = 720;
+}
+#pragma endregion
+
+#pragma region Direct3D Resources
+// These are the resources that depend on the device.
+void Game::CreateDeviceDependentResources()
+{
+    auto device = m_deviceResources->GetD3DDevice();
+
+#if defined(_XBOX_ONE) && defined(_TITLE)
+    m_graphicsMemory = std::make_unique<GraphicsMemory>(device, m_deviceResources->GetBackBufferCount());
+#endif
+
+    m_hdrScene->SetDevice(device);
+
+    m_toneMap = std::make_unique<ToneMapPostProcess>(device);
+    m_toneMap->SetOperator(ToneMapPostProcess::ACESFilmic);
+    m_toneMap->SetTransferFunction(ToneMapPostProcess::SRGB);
+
+#if defined(_XBOX_ONE) && defined(_TITLE)
+    m_toneMap->SetMRTOutput(true);
+#endif
+
+    // TODO: Initialize device dependent objects here (independent of window size).
+}
+
+// Allocate all memory resources that change on a window SizeChanged event.
+void Game::CreateWindowSizeDependentResources()
+{
+    auto size = m_deviceResources->GetOutputSize();
+
+    // Set windows size for HDR.
+    m_hdrScene->SetWindow(size);
+
+    m_toneMap->SetHDRSourceTexture(m_hdrScene->GetShaderResourceView());
+
+    // TODO: Initialize windows-size dependent objects here.
+}
+
+#if !defined(_XBOX_ONE) || !defined(_TITLE)
+void Game::OnDeviceLost()
+{
+    // TODO: Add Direct3D resource cleanup here.
+
+    m_toneMap.reset();
+
+    m_hdrScene->ReleaseDevice();
+}
+
+void Game::OnDeviceRestored()
+{
+    CreateDeviceDependentResources();
+
+    CreateWindowSizeDependentResources();
+}
+#endif
+#pragma endregion
--- PBRModelTest/Game.h
+++ PBRModelTest/Game.h
@@ -1,7 +1,7 @@
 //--------------------------------------------------------------------------------------
 // File: Game.h
 //
-// Developer unit test for DirectXTK PBR Effect Test
+// Developer unit test for DirectXTK PBR Model Test
 //
 // THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF
 // ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO
@@ -73,7 +73,7 @@ public:
 
     // Properties
     void GetDefaultSize( int& width, int& height ) const;
-    const wchar_t* GetAppName() const { return L"PBRTest (DirectX 11)"; }
+    const wchar_t* GetAppName() const { return L"PBRModelTest (DirectX 11)"; }
     bool RequestHDRMode() const { return m_deviceResources ? (m_deviceResources->GetDeviceOptions() & DX::DeviceResources::c_EnableHDR) != 0 : false; }
 
 private:
@@ -96,54 +96,12 @@ private:
     std::unique_ptr<DirectX::GamePad>       m_gamePad;
     std::unique_ptr<DirectX::Keyboard>      m_keyboard;
 
-    DirectX::GamePad::ButtonStateTracker    m_gamePadButtons;
-    DirectX::Keyboard::KeyboardStateTracker m_keyboardButtons;
-
     // DirectXTK Test Objects
 #if defined(_XBOX_ONE) && defined(_TITLE)
     std::unique_ptr<DirectX::GraphicsMemory> m_graphicsMemory;
 #endif
-    std::unique_ptr<DirectX::CommonStates>  m_states;
 
     // HDR resources
     std::unique_ptr<DirectX::ToneMapPostProcess>    m_toneMap;
     std::unique_ptr<DX::RenderTexture>              m_hdrScene;
-
-    // Test geometry
-    Microsoft::WRL::ComPtr<ID3D11Buffer>        m_vertexBuffer;
-    Microsoft::WRL::ComPtr<ID3D11Buffer>        m_indexBuffer;
-    Microsoft::WRL::ComPtr<ID3D11InputLayout>   m_inputLayoutNM;
-    Microsoft::WRL::ComPtr<ID3D11InputLayout>   m_inputLayoutPBR;
-    Microsoft::WRL::ComPtr<ID3D11InputLayout>   m_inputLayoutDBG;
-    UINT                                        m_indexCount;
-
-    Microsoft::WRL::ComPtr<ID3D11Buffer>        m_vertexBufferCube;
-    Microsoft::WRL::ComPtr<ID3D11Buffer>        m_indexBufferCube;
-    Microsoft::WRL::ComPtr<ID3D11InputLayout>   m_inputLayoutCube;
-    UINT                                        m_indexCountCube;
-
-    // Test materials
-    std::unique_ptr<DirectX::NormalMapEffect>       m_normalMapEffect;
-    std::unique_ptr<DirectX::PBREffect>             m_pbr;
-    std::unique_ptr<DirectX::PBREffect>             m_pbrCube;
-    std::unique_ptr<DirectX::DebugEffect>           m_debug;
-
-    static const size_t s_nMaterials = 3;
-    static const size_t s_nIBL = 3;
-
-    Microsoft::WRL::ComPtr<ID3D11ShaderResourceView>    m_baseColor[s_nMaterials];
-    Microsoft::WRL::ComPtr<ID3D11ShaderResourceView>    m_normalMap[s_nMaterials];
-    Microsoft::WRL::ComPtr<ID3D11ShaderResourceView>    m_rma[s_nMaterials];
-    Microsoft::WRL::ComPtr<ID3D11ShaderResourceView>    m_emissiveMap[s_nMaterials];
-    Microsoft::WRL::ComPtr<ID3D11ShaderResourceView>    m_radianceIBL[s_nIBL];
-    Microsoft::WRL::ComPtr<ID3D11ShaderResourceView>    m_irradianceIBL[s_nIBL];
-
-    uint32_t m_ibl;
-    bool m_spinning;
-    bool m_showDebug;
-    DirectX::DebugEffect::Mode m_debugMode;
-    float m_pitch;
-    float m_yaw;
-
-    void CycleDebug();
 };
