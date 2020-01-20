f9037e7e79e6953a84e9a38c728f4f602f4ffb84
Chuck Walbourn <chuckw@microsoft.com>
2020-01-19 23:20:19 -0800

    Minor code review

--- fuzzloaders/fuzzloaders.cpp
+++ fuzzloaders/fuzzloaders.cpp
@@ -49,13 +49,13 @@ namespace
 {
     struct handle_closer { void operator()(HANDLE h) { if (h) CloseHandle(h); } };
 
-    typedef std::unique_ptr<void, handle_closer> ScopedHandle;
+    using ScopedHandle = std::unique_ptr<void, handle_closer>;
 
     inline HANDLE safe_handle(HANDLE h) { return (h == INVALID_HANDLE_VALUE) ? nullptr : h; }
 
     struct find_closer { void operator()(HANDLE h) { assert(h != INVALID_HANDLE_VALUE); if (h) FindClose(h); } };
 
-    typedef std::unique_ptr<void, find_closer> ScopedFindHandle;
+    using ScopedFindHandle = std::unique_ptr<void, find_closer>;
 }
 
 
