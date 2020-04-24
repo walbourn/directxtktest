e4f78b776355820be70b807b10d8159a766eac59
Chuck Walbourn <chuckw@microsoft.com>
2020-04-24 02:21:51 -0700

    Added ADPCM test

--- StreamingAudioTest/StreamingAudioTest.cpp
+++ StreamingAudioTest/StreamingAudioTest.cpp
@@ -31,6 +31,9 @@
 
 #include <stdio.h>
 
+#define TEST_PCM
+#define TEST_ADPCM
+
 using namespace DirectX;
 
 //--------------------------------------------------------------------------------------
@@ -176,6 +179,7 @@ int __cdecl main()
         return 1;
     }
 
+#ifdef TEST_PCM
     { // PCM WaveBank
         auto wb = std::make_unique<WaveBank>(audEngine.get(), L"WaveBank.xwb");
         printf("\n\nINFO: Loaded wavebank.xwb\n");
@@ -201,23 +205,27 @@ int __cdecl main()
         {
             UPDATE
 
-            if (GetAsyncKeyState(VK_ESCAPE))
-            {
-                while (GetAsyncKeyState(VK_ESCAPE))
-                    Sleep(10);
-                break;
-            }
+                if (GetAsyncKeyState(VK_ESCAPE))
+                {
+                    while (GetAsyncKeyState(VK_ESCAPE))
+                        Sleep(10);
+                    break;
+                }
 
             printf(".");
             Sleep(200);
 
             ULONGLONG tick = GetTickCount64();
 
-            if (tick > startTick + 43000)
+            if (tick > startTick + 60000)
+            {
+                printf("<timeout>\n");
                 break;
+            }
         }
 
         stream1->Stop();
+        printf("<done>\n");
 
         auto stream2 = wb->CreateStreamInstance(2u);
 
@@ -226,31 +234,133 @@ int __cdecl main()
 
         startTick = GetTickCount64();
 
+        bool exitloop = true;
         while (stream2->GetState() == PLAYING)
         {
             UPDATE
 
-            if (GetAsyncKeyState(VK_ESCAPE))
-            {
-                while (GetAsyncKeyState(VK_ESCAPE))
-                    Sleep(10);
-                break;
-            }
+                if (GetAsyncKeyState(VK_ESCAPE))
+                {
+                    while (GetAsyncKeyState(VK_ESCAPE))
+                        Sleep(10);
+                    break;
+                }
 
             printf(".");
             Sleep(200);
 
             ULONGLONG tick = GetTickCount64();
 
-            if (tick > startTick + 45000)
+            if (tick > startTick + 80000)
+            {
+                printf("<timeout>\n");
                 break;
+            }
+            else if (exitloop && (tick > startTick + 45000))
+            {
+                printf("<breaking loop>");
+                stream2->Stop(false);
+                exitloop = false;
+            }
         }
 
         stream2->Stop();
-        printf("\n");
+        printf("<done>\n");
 
         // TODO - Pause, Resume, Volume, Pan
     }
+#endif
 
-    printf("\tPASS\n");
+#ifdef TEST_ADPCM
+    {
+        // ADPCM WaveBank
+        auto wb = std::make_unique<WaveBank>(audEngine.get(), L"WaveBankADPCM.xwb");
+        printf("\n\nINFO: Loaded WaveBankADPCM.xwb\n");
+
+        char buff[64] = {};
+        auto wfx = reinterpret_cast<WAVEFORMATEX*>(&buff);
+
+        for (uint32_t j = 0; j < 3; ++j)
+        {
+            printf("\tIndex #%u (%zu bytes, %zu samples, %zu ms)\n",
+                j, wb->GetSampleSizeInBytes(j), wb->GetSampleDuration(j), wb->GetSampleDurationMS(j));
+            dump_wfx(wb->GetFormat(j, wfx, 64));
+        }
+
+        auto stream1 = wb->CreateStreamInstance(0u);
+
+        printf("Playing #0: ");
+        stream1->Play();
+
+        ULONGLONG startTick = GetTickCount64();
+
+        while (stream1->GetState() == PLAYING)
+        {
+            UPDATE
+
+                if (GetAsyncKeyState(VK_ESCAPE))
+                {
+                    while (GetAsyncKeyState(VK_ESCAPE))
+                        Sleep(10);
+                    break;
+                }
+
+            printf(".");
+            Sleep(200);
+
+            ULONGLONG tick = GetTickCount64();
+
+            if (tick > startTick + 60000)
+            {
+                printf("<timeout>\n");
+                break;
+            }
+        }
+
+        stream1->Stop();
+        printf("<done>\n");
+
+        auto stream2 = wb->CreateStreamInstance(2u);
+
+        printf("\n\nPlaying #2 (looped): ");
+        stream2->Play(true);
+
+        startTick = GetTickCount64();
+
+        bool exitloop = true;
+        while (stream2->GetState() == PLAYING)
+        {
+            UPDATE
+
+                if (GetAsyncKeyState(VK_ESCAPE))
+                {
+                    while (GetAsyncKeyState(VK_ESCAPE))
+                        Sleep(10);
+                    break;
+                }
+
+            printf(".");
+            Sleep(200);
+
+            ULONGLONG tick = GetTickCount64();
+
+            if (tick > startTick + 80000)
+            {
+                printf("<timeout>\n");
+                break;
+            }
+            else if (exitloop && (tick > startTick + 45000))
+            {
+                printf("<breaking loop>");
+                stream2->Stop(false);
+                exitloop = false;
+            }
+        }
+
+        stream2->Stop();
+        printf("<done>\n");
+
+        // TODO - Pause, Resume
+    }
+#endif
 }
