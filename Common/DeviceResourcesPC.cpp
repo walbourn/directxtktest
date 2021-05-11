da12871c33ce5937f9dca60ac96bd7da11e24a64
Chuck Walbourn <chuckw@windows.microsoft.com>
2021-05-11 13:28:33 -0700

    Minor code review

--- Common/DeviceResourcesPC.cpp
+++ Common/DeviceResourcesPC.cpp
@@ -648,7 +648,6 @@ void DeviceResources::UpdateColorSpace()
 
     bool isDisplayHDR10 = false;
 
-#if defined(NTDDI_WIN10_RS2)
     if (m_swapChain)
     {
         ComPtr<IDXGIOutput> output;
@@ -668,7 +667,6 @@ void DeviceResources::UpdateColorSpace()
             }
         }
     }
-#endif
 
     if ((m_options & c_EnableHDR) && isDisplayHDR10)
     {
--- Common/DeviceResourcesUWP.cpp
+++ Common/DeviceResourcesUWP.cpp
@@ -775,7 +775,6 @@ void DeviceResources::UpdateColorSpace()
 
     bool isDisplayHDR10 = false;
 
-#if defined(NTDDI_WIN10_RS2)
     if (m_swapChain)
     {
         ComPtr<IDXGIOutput> output;
@@ -795,7 +794,6 @@ void DeviceResources::UpdateColorSpace()
             }
         }
     }
-#endif
 
     if ((m_options & c_EnableHDR) && isDisplayHDR10)
     {
