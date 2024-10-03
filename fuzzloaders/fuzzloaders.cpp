4b8efa06f639e164b7f894e0d84b16f6b17c1439
Chuck Walbourn <walbourn@users.noreply.github.com>
2024-10-03 15:45:59 -0700

    Refactor fuzzloaders to use CmdLineHelpers.h (#47)

--- fuzzloaders/fuzzloaders.cpp
+++ fuzzloaders/fuzzloaders.cpp
@@ -43,21 +43,17 @@
 
 #include <wrl\client.h>
 
+#define TOOL_VERSION 0
+#include "CmdLineHelpers.h"
+
+using namespace Helpers;
 using Microsoft::WRL::ComPtr;
+
 
 namespace
 {
-#ifdef FUZZING_BUILD_MODE
-    struct handle_closer { void operator()(HANDLE h) { if (h) CloseHandle(h); } };
-
-    using ScopedHandle = std::unique_ptr<void, handle_closer>;
-#endif
-
-    inline HANDLE safe_handle(HANDLE h) { return (h == INVALID_HANDLE_VALUE) ? nullptr : h; }
-
-    struct find_closer { void operator()(HANDLE h) { assert(h != INVALID_HANDLE_VALUE); if (h) FindClose(h); } };
-
-    using ScopedFindHandle = std::unique_ptr<void, find_closer>;
+    const wchar_t* g_ToolName = L"fuzzloaders";
+    const wchar_t* g_Description = L"Microsoft (R) DirectX Tool Kit for DX11 File Fuzzing Harness";
 
 #ifndef FUZZING_BUILD_MODE
 
@@ -65,7 +61,7 @@ namespace
     //////////////////////////////////////////////////////////////////////////////
     //////////////////////////////////////////////////////////////////////////////
 
-    enum OPTIONS
+    enum OPTIONS : uint32_t
     {
         OPT_RECURSIVE = 1,
         OPT_DDS,
@@ -77,22 +73,11 @@ namespace
 
     static_assert(OPT_MAX <= 32, "dwOptions is a DWORD bitfield");
 
-    struct SConversion
-    {
-        wchar_t szSrc[MAX_PATH];
-    };
-
-    struct SValue
-    {
-        LPCWSTR pName;
-        DWORD dwValue;
-    };
-
     //////////////////////////////////////////////////////////////////////////////
     //////////////////////////////////////////////////////////////////////////////
     //////////////////////////////////////////////////////////////////////////////
 
-    const SValue g_pOptions [] =
+    const SValue<uint32_t> g_pOptions [] =
     {
         { L"r",         OPT_RECURSIVE },
         { L"dds",       OPT_DDS },
@@ -106,107 +91,20 @@ namespace
     //////////////////////////////////////////////////////////////////////////////
     //////////////////////////////////////////////////////////////////////////////
 
-#ifdef _PREFAST_
-#pragma prefast(disable : 26018, "Only used with static internal arrays")
-#endif
-
-    DWORD LookupByName(const wchar_t *pName, const SValue *pArray)
-    {
-        while (pArray->pName)
-        {
-            if (!_wcsicmp(pName, pArray->pName))
-                return pArray->dwValue;
-
-            pArray++;
-        }
-
-        return 0;
-    }
-
-    void SearchForFiles(const wchar_t* path, std::list<SConversion>& files, bool recursive)
-    {
-        // Process files
-        WIN32_FIND_DATA findData = {};
-        ScopedFindHandle hFile(safe_handle(FindFirstFileExW(path,
-            FindExInfoBasic, &findData,
-            FindExSearchNameMatch, nullptr,
-            FIND_FIRST_EX_LARGE_FETCH)));
-        if (hFile)
-        {
-            for (;;)
-            {
-                if (!(findData.dwFileAttributes & (FILE_ATTRIBUTE_HIDDEN | FILE_ATTRIBUTE_SYSTEM | FILE_ATTRIBUTE_DIRECTORY)))
-                {
-                    wchar_t drive[_MAX_DRIVE] = {};
-                    wchar_t dir[_MAX_DIR] = {};
-                    _wsplitpath_s(path, drive, _MAX_DRIVE, dir, _MAX_DIR, nullptr, 0, nullptr, 0);
-
-                    SConversion conv;
-                    _wmakepath_s(conv.szSrc, drive, dir, findData.cFileName, nullptr);
-                    files.push_back(conv);
-                }
-
-                if (!FindNextFile(hFile.get(), &findData))
-                    break;
-            }
-        }
-
-        // Process directories
-        if (recursive)
-        {
-            wchar_t searchDir[MAX_PATH] = {};
-            {
-                wchar_t drive[_MAX_DRIVE] = {};
-                wchar_t dir[_MAX_DIR] = {};
-                _wsplitpath_s(path, drive, _MAX_DRIVE, dir, _MAX_DIR, nullptr, 0, nullptr, 0);
-                _wmakepath_s(searchDir, drive, dir, L"*", nullptr);
-            }
-
-            hFile.reset(safe_handle(FindFirstFileExW(searchDir,
-                FindExInfoBasic, &findData,
-                FindExSearchLimitToDirectories, nullptr,
-                FIND_FIRST_EX_LARGE_FETCH)));
-            if (!hFile)
-                return;
-
-            for (;;)
-            {
-                if (findData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
-                {
-                    if (findData.cFileName[0] != L'.')
-                    {
-                        wchar_t subdir[MAX_PATH] = {};
-
-                        {
-                            wchar_t drive[_MAX_DRIVE] = {};
-                            wchar_t dir[_MAX_DIR] = {};
-                            wchar_t fname[_MAX_FNAME] = {};
-                            wchar_t ext[_MAX_FNAME] = {};
-                            _wsplitpath_s(path, drive, dir, fname, ext);
-                            wcscat_s(dir, findData.cFileName);
-                            _wmakepath_s(subdir, drive, dir, fname, ext);
-                        }
-
-                        SearchForFiles(subdir, files, recursive);
-                    }
-                }
-
-                if (!FindNextFile(hFile.get(), &findData))
-                    break;
-            }
-        }
-    }
-
     void PrintUsage()
     {
-        wprintf(L"DirectX Tool Kit for DX11\n\n");
-        wprintf(L"Usage: fuzzloaders <options> <files>\n");
-        wprintf(L"\n");
-        wprintf(L"   -r                  wildcard filename search is recursive\n");
-        wprintf(L"   -dds                force use of DDSTextureLoader\n");
-        wprintf(L"   -wav                force use of WAVFileReader\n");
-        wprintf(L"   -wic                force use of WICTextureLoader\n");
-        wprintf(L"   -xwb                force use of WaveBankReader\n");
+        PrintLogo(false, g_ToolName, g_Description);
+
+        static const wchar_t* const s_usage =
+            L"Usage: fuzzloaders <options> <files>\n"
+            L"\n"
+            L"   -r                  wildcard filename search is recursive\n"
+            L"   -dds                force use of DDSTextureLoader\n"
+            L"   -wav                force use of WAVFileReader\n"
+            L"   -wic                force use of WICTextureLoader\n"
+            L"   -xwb                force use of WaveBankReader\n";
+
+        wprintf(L"%ls", s_usage);
     }
 
 #endif // !FUZZING_BUILD_MODE
@@ -314,7 +212,7 @@ int __cdecl wmain(_In_ int argc, _In_z_count_(argc) wchar_t* argv[])
         else if (wcspbrk(pArg, L"?*") != nullptr)
         {
             size_t count = conversion.size();
-            SearchForFiles(pArg, conversion, (dwOptions & (1 << OPT_RECURSIVE)) != 0);
+            SearchForFiles(pArg, conversion, (dwOptions & (1 << OPT_RECURSIVE)) != 0, nullptr);
             if (conversion.size() <= count)
             {
                 wprintf(L"No matching files found for %ls\n", pArg);
@@ -323,8 +221,8 @@ int __cdecl wmain(_In_ int argc, _In_z_count_(argc) wchar_t* argv[])
         }
         else
         {
-            SConversion conv;
-            wcscpy_s(conv.szSrc, MAX_PATH, pArg);
+            SConversion conv = {};
+            conv.szSrc = pArg;
 
             conversion.push_back(conv);
         }
@@ -349,7 +247,7 @@ int __cdecl wmain(_In_ int argc, _In_z_count_(argc) wchar_t* argv[])
     for (auto& pConv : conversion)
     {
         wchar_t ext[_MAX_EXT];
-        _wsplitpath_s(pConv.szSrc, nullptr, 0, nullptr, 0, nullptr, 0, ext, _MAX_EXT);
+        _wsplitpath_s(pConv.szSrc.c_str(), nullptr, 0, nullptr, 0, nullptr, 0, ext, _MAX_EXT);
         bool isdds = (_wcsicmp(ext, L".dds") == 0);
         bool iswav = (_wcsicmp(ext, L".wav") == 0);
         bool isxwb = (_wcsicmp(ext, L".xwb") == 0);
@@ -378,19 +276,19 @@ int __cdecl wmain(_In_ int argc, _In_z_count_(argc) wchar_t* argv[])
 
         // Load source image
 #ifdef _DEBUG
-        OutputDebugStringW(pConv.szSrc);
+        OutputDebugStringW(pConv.szSrc.c_str());
         OutputDebugStringA("\n");
 #endif
 
         ComPtr<ID3D11Resource> tex;
         if (usedds)
         {
-            hr = DirectX::CreateDDSTextureFromFileEx(device.Get(), pConv.szSrc, 0,
+            hr = DirectX::CreateDDSTextureFromFileEx(device.Get(), pConv.szSrc.c_str(), 0,
                 D3D11_USAGE_STAGING, 0, D3D11_CPU_ACCESS_WRITE, 0,
                 DirectX::DDS_LOADER_DEFAULT, tex.GetAddressOf(), nullptr, nullptr);
             if (hr == HRESULT_FROM_WIN32(ERROR_FILE_NOT_FOUND))
             {
-                wprintf(L"ERROR: DDSTexture file not not found:\n%ls\n", pConv.szSrc);
+                wprintf(L"ERROR: DDSTexture file not not found:\n%ls\n", pConv.szSrc.c_str());
                 return 1;
             }
             else if (FAILED(hr) && hr != E_INVALIDARG && hr != HRESULT_FROM_WIN32(ERROR_NOT_SUPPORTED) && hr != E_OUTOFMEMORY && hr != HRESULT_FROM_WIN32(ERROR_HANDLE_EOF) && (hr != E_FAIL || (hr == E_FAIL && isdds)))
@@ -411,10 +309,10 @@ int __cdecl wmain(_In_ int argc, _In_z_count_(argc) wchar_t* argv[])
         {
             std::unique_ptr<uint8_t[]> data;
             DirectX::WAVData result = {};
-            hr = DirectX::LoadWAVAudioFromFileEx(pConv.szSrc, data, result);
+            hr = DirectX::LoadWAVAudioFromFileEx(pConv.szSrc.c_str(), data, result);
             if (hr == HRESULT_FROM_WIN32(ERROR_FILE_NOT_FOUND))
             {
-                wprintf(L"ERROR: WAVAudio file not not found:\n%ls\n", pConv.szSrc);
+                wprintf(L"ERROR: WAVAudio file not not found:\n%ls\n", pConv.szSrc.c_str());
                 return 1;
             }
             else if (FAILED(hr) && hr != E_INVALIDARG && hr != HRESULT_FROM_WIN32(ERROR_NOT_SUPPORTED) && hr != E_OUTOFMEMORY && hr != HRESULT_FROM_WIN32(ERROR_INVALID_DATA) && hr != HRESULT_FROM_WIN32(ERROR_HANDLE_EOF) && (hr != E_FAIL || (hr == E_FAIL && iswav)))
@@ -434,10 +332,10 @@ int __cdecl wmain(_In_ int argc, _In_z_count_(argc) wchar_t* argv[])
         else if (usexwb)
         {
             auto wb = std::make_unique<DirectX::WaveBankReader>();
-            hr = wb->Open(pConv.szSrc);
+            hr = wb->Open(pConv.szSrc.c_str());
             if (hr == HRESULT_FROM_WIN32(ERROR_FILE_NOT_FOUND))
             {
-                wprintf(L"ERROR: XWBAudio file not not found:\n%ls\n", pConv.szSrc);
+                wprintf(L"ERROR: XWBAudio file not not found:\n%ls\n", pConv.szSrc.c_str());
                 return 1;
             }
             else if (FAILED(hr) && hr != E_INVALIDARG && hr != HRESULT_FROM_WIN32(ERROR_NOT_SUPPORTED) && hr != E_OUTOFMEMORY && hr != HRESULT_FROM_WIN32(ERROR_HANDLE_EOF) && (hr != E_FAIL || (hr == E_FAIL && isxwb)))
@@ -462,10 +360,10 @@ int __cdecl wmain(_In_ int argc, _In_z_count_(argc) wchar_t* argv[])
         }
         else
         {
-            hr = DirectX::CreateWICTextureFromFileEx(device.Get(), pConv.szSrc, 0, D3D11_USAGE_STAGING, 0, D3D11_CPU_ACCESS_WRITE, 0, DirectX::WIC_LOADER_DEFAULT, tex.GetAddressOf(), nullptr);
+            hr = DirectX::CreateWICTextureFromFileEx(device.Get(), pConv.szSrc.c_str(), 0, D3D11_USAGE_STAGING, 0, D3D11_CPU_ACCESS_WRITE, 0, DirectX::WIC_LOADER_DEFAULT, tex.GetAddressOf(), nullptr);
             if (hr == HRESULT_FROM_WIN32(ERROR_FILE_NOT_FOUND))
             {
-                wprintf(L"ERROR: WICTexture file not found:\n%ls\n", pConv.szSrc);
+                wprintf(L"ERROR: WICTexture file not found:\n%ls\n", pConv.szSrc.c_str());
                 return 1;
             }
             else if (FAILED(hr) && hr != E_INVALIDARG && hr != HRESULT_FROM_WIN32(ERROR_NOT_SUPPORTED) && hr != WINCODEC_ERR_COMPONENTNOTFOUND && hr != E_OUTOFMEMORY && hr != WINCODEC_ERR_BADHEADER)
