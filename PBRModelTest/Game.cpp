b1a06c1c645874039dacdb4466c18f165815641b
Chuck Walbourn <chuckw@microsoft.com>
2025-08-19 12:01:12 -0700

    Test fix

--- PBRModelTest/Game.cpp
+++ PBRModelTest/Game.cpp
@@ -681,6 +681,7 @@ void Game::CreateDeviceDependentResources()
 
     static const wchar_t* s_searchFolders[] =
     {
+        L"..\\PBRTest",
         L"PBRModelTest",
         L"PBRTest",
         nullptr
